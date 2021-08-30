323d4453d532157b2b60496e8892ae29e2b50923
Dobro <matematikaigor@gmail.com>
2021-08-31 00:02:09 +0400

    change test now it works via mocking. and add fixture

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -2,16 +2,19 @@
 """Tests."""
 
 import tempfile
-
+import requests
 from page_loader.page_loader import download
 
 
-def test_download_path():
+def test_download(requests_mock):
     """Tests generated files path of download function.
 
     Returns answer of assert.
     """
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        file_path = download('https://ru.hexlet.io/courses', tmpdirname)
-        expected_path = '{0}/ru-hexlet-io-courses.html'.format(tmpdirname)
-        assert file_path == expected_path
+    with tempfile.TemporaryDirectory() as tmpdir:
+        with open('tests/fixtures/response.html', 'r') as fixture:
+            requests_mock.get('https://ru.hexlet.io/courses', text='html_page')
+            file_path = download('https://ru.hexlet.io/courses', tmpdir)
+            assert file_path == '{0}/ru-hexlet-io-courses.html'.format(tmpdir)
+            with open(file_path, 'r') as html_file:
+                assert html_file.read() == fixture.read()
