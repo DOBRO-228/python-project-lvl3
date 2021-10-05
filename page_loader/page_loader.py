c5fa23b5310708132659f43e9bd687f7c443c6c7
DOBRO-228 <matematikaigor@gmail.com>
2021-10-06 01:16:56 +0300

    refactor

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -3,11 +3,11 @@
 """Page loader cli."""
 
 import argparse
+import logging
 import os
 import pathlib
 import sys
 from urllib.parse import urlparse
-
 
 import requests
 from bs4 import BeautifulSoup
@@ -17,11 +17,9 @@ from page_loader.path_formatter import path_formatter, path_to_file
 def download(url, output=None):
     if output is None:
         output = os.getcwd()
+    check_folder(output)
     path_builder = path_formatter(url, output)
-    pathlib.Path(path_builder['path_to_files']).mkdir(
-        parents=True, exist_ok=True,
-    )
-    html = requests.get(path_builder['original_url']).text
+    html = request_wrapper(url).text
     soup = BeautifulSoup(html, 'html.parser')
     download_files(path_builder, soup)
     with open(path_builder['path_to_html'], 'w') as html_file:
@@ -30,16 +28,20 @@ def download(url, output=None):
 
 
 def download_files(path_builder, soup):
+    pathlib.Path(path_builder['path_to_files']).mkdir(
+        parents=False, exist_ok=True,
+    )
+    sources = {
+        'img': 'src',
+        'script': 'src',
+        'link': 'href',
+    }
     for html_elem in soup.find_all(['img', 'script', 'link']):
-        sources = {
-            'img': 'src',
-            'script': 'src',
-            'link': 'href',
-        }
         file_src = html_elem.get(sources[html_elem.name])
-        if file_src is not None:
-            if file_src.startswith(('/', path_builder['scheme_with_host'])):
-                html_elem[sources[html_elem.name]] = download_file(file_src, path_builder)
+        if file_src is not None and file_src.startswith(('/', path_builder['scheme_with_host'])):
+            html_elem[sources[html_elem.name]] = download_file(
+                file_src, path_builder,
+            )
 
 
 def download_file(src, path_builder):
@@ -54,8 +56,25 @@ def download_file(src, path_builder):
     if not extension:
         src = '{0}.html'.format(src)
     with open(path_to_file(src, path_builder), 'wb') as inner_file:
-        inner_file.write(requests.get(url_to_download).content)
+        inner_file.write(request_wrapper(url_to_download).content)
     return path_to_file(src, path_builder)
+
+
+def check_folder(path):
+    if not os.path.exists(path):
+        raise FileNotFoundError("{0} - Folder doesn't exist".format(path))
+    elif not os.path.isdir(path):
+        raise NotADirectoryError('{0} - You need to choose a folder, not a file'.format(path))
+    elif not os.access(path, os.W_OK):
+        raise PermissionError("{0} - You don't have permissions to write into this folder".format(path))
+
+
+def request_wrapper(url):
+    res = requests.get(url)
+    not_expected_status = 399
+    if res.status_code > not_expected_status:
+        res.raise_for_status()
+    return res
 
 
 def main():
@@ -72,7 +91,8 @@ def main():
     group = parser.add_argument_group('Options')
     group.add_argument(
         '-o',
-        metavar='--output',
+        metavar=('--output', '[dir]'),
+        nargs=2,
         help='output dir (default: "/app")',
         default=os.getcwd(),
     )
@@ -83,7 +103,21 @@ def main():
         help='display help for command',
     )
     args = parser.parse_args()
-    file_path = '{0}\n'.format(download(args.url, args.o))
+    logging.basicConfig(
+        format='%(levelname)s: %(folder)s - %(message)s',
+        level=logging.ERROR,
+        stream=sys.stderr,
+    )
+    try:
+        check_folder(args.o)
+    except OSError as error:
+        logging.error(error, extra={'folder': args.o})
+        sys.exit(1)
+    try:
+        file_path = '{0}\n'.format(download(args.url, args.o))
+    except requests.exceptions.RequestException as request_error:
+        logging.error(request_error, extra={'folder': ''})
+        sys.exit(1)
     sys.stdout.write(file_path)
 
 
